3ffdb677215343954025e5d57c7fa89da8055ba1
David Herrera <herrera.dl@gmail.com>
2012-02-27 17:24:25 -0800

    Add jQuery to display label via radio button

--- js/script.js
+++ js/script.js
@@ -3,6 +3,93 @@
 */
 
 
+/**
+ * jQuery to update field as displayed in <pre>
+ *
+ * This probably could be more DRY-ish, but there are so few fields, and so
+ * many small differences among the fields, that individually controlling them
+ * seems easier right now
+ */
 
+/* ident */
+$("input.ident").keyup(function () {
+  var value = $(this).val();
+  $("span.ident").text(value);
+}).keyup();
 
+/* selector */
+$("input.selector").keyup(function () {
+  var value = $(this).val();
+  $("span.selector").text(value);
+}).keyup();
 
+/* priority */
+$("input.priority").keyup(function () {
+  var value = $(this).val();
+  $("span.priority").text(value);
+}).keyup();
+
+/* category */
+$("input.category").keyup(function () {
+  var value = $(this).val();
+  $("span.category").text(value);
+}).keyup();
+
+/* format */
+$("input.format").keyup(function () {
+  var value = $(this).val();
+  $("span.format").text(value);
+}).keyup();
+
+/* slug */
+$("input.slug").keyup(function () {
+  var value = $(this).val();
+  $("span.slug").text(value);
+}).keyup();
+
+/* label */
+$("input.label").click(function () {
+  var value = $('input:radio[name=label]:checked').val();
+  $("span.label").text(value);
+}).keyup();
+
+/* headline */
+$("input.headline").keyup(function () {
+  var value = $(this).val();
+  $("span.headline").text(value);
+}).keyup();
+
+/* wordcount */
+$("input.wordcount").keyup(function () {
+  var value = $(this).val();
+  $("span.wordcount").text(value);
+}).keyup();
+
+/* photosof */
+
+/* categories */
+$("input.categories").keyup(function () {
+  var value = $(this).val();
+  $("span.categories").text(value);
+}).keyup();
+
+/**
+ * byline
+ *
+ * automatically uppercase
+ *
+ * @url http://jquerybyexample.blogspot.com/2010/09/jquery-code-convert-string-value-to.html
+ */
+$("input.byline").keyup(function () {
+  var value = $(this).val().toUpperCase();
+  $("span.byline").text(value);
+}).keyup();
+
+/**
+ * year
+ *
+ * automatically insert current year
+ *
+ * @url http://forum.jquery.com/topic/get-current-year
+ */
+$("span.year").text( (new Date).getFullYear() );
